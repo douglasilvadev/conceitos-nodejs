1a62953e55f1c8d3bcd66cbe4f9bad2945c0a7ae
Douglas Silva <douglasilva14@terra.com.br>
2020-04-09 18:29:56 -0300

    Criado as rotas, middleware, TDD e Readme

---
+++ Readme.md
@@ -0,0 +1,47 @@
+<img alt="GoStack" src="https://storage.googleapis.com/golden-wind/bootcamp-gostack/header-desafios.png" />
+
+<h3 align="center">
+  Desafio 02: Conceitos do Node.js
+</h3>
+
+<p align="center">“Não espere para plantar, apenas tenha paciência para colher”!</blockquote>
+
+<p align="center">
+  <img alt="GitHub language count" src="https://img.shields.io/github/languages/count/douglassp/conceitos-nodejs?color=%2304D361">
+
+  <a href="https://rocketseat.com.br">
+    <img alt="Altered by Douglas Silva" src="https://img.shields.io/badge/altered%20by-Douglas_Silva-%2304D361">
+  </a>
+
+  <img alt="License" src="https://img.shields.io/badge/license-MIT-%2304D361">
+
+  <a href="https://github.com/douglassp/conceitos-nodejs/stargazers">
+    <img alt="Stargazers" src="https://img.shields.io/github/stars/douglassp/conceitos-nodejs?style=social">
+  </a>
+</p>
+
+<p align="center">
+  <a href="#rocket-sobre-o-desafio">Sobre o desafio</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+
+
+## :rocket: Sobre o desafio
+
+Aplicação feita para treinar a parte conceitual do primeiro módulo de Node.js ensinado pela @Rocketseat
+
+Desenvolvido para armazenar repositórios de portfólio onde é permitido a criação, listagem, atualização e remoção dos repositórios além de permitir que os repositórios possam receber "likes".
+
+
+### Template da aplicação
+
+O template utilizado está disponível na seguinte url: **[Acessar Template](https://github.com/Rocketseat/gostack-template-conceitos-nodejs)**
+
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE.md) para mais detalhes.
+
+---
+
+Feito com 💜 by Rocketseat e alterado by Douglas Silva 💪 :wave: [Entre na nossa comunidade!](https://discordapp.com/invite/gCRAFhc)
--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
-const express = require("express");
-const cors = require("cors");
+/* eslint arrow-parens: ["error", "as-needed"] */
 
-// const { uuid } = require("uuidv4");
+const express = require('express');
+const cors = require('cors');
+
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
@@ -10,24 +12,84 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+function validateRepositorytId(req, res, next) {
+  const { id } = req.params;
+
+  if (!isUuid(id)) {
+    return res.status(400).json({ error: 'Invalid repository ID.' });
+  }
+  return next();
+}
+
+app.use('/repositories/:id', validateRepositorytId);
+
+app.get('/repositories', (req, res) => res.json(repositories));
+
+app.post('/repositories', (req, res) => {
+  const { title, url, techs } = req.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return res.json(repository);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.put('/repositories/:id', (req, res) => {
+  const { id } = req.params;
+  const { title, url, techs } = req.body;
+
+  const alteredRepository = repositories.find(repository => repository.id === id);
+
+  if (!alteredRepository) {
+    return res.status(400).json({ error: 'Repository not found' });
+  }
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories[alteredRepository] = repository;
+
+  return res.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.delete('/repositories/:id', (req, res) => {
+  const { id } = req.params;
+
+  const deletedRepository = repositories.findIndex(repository => repository.id === id);
+
+  if (deletedRepository < 0) {
+    return res.status(400).json({ error: 'Repository not found' });
+  }
+
+  repositories.splice(deletedRepository, 1);
+
+  return res.status(204).send();
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
-});
+app.post('/repositories/:id/like', (req, res) => {
+  const { id } = req.params;
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const repository = repositories.find(repository => repository.id === id);
+
+  if (!repository) {
+    return res.status(400).send();
+  }
+
+  repository.likes += 1;
+
+  return res.json(repository);
 });
 
 module.exports = app;
